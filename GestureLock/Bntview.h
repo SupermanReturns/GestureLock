81ca5261645495addddc38f043094e18ccf5263e
Superman <703353105@qq.com>
2018-07-11 14:49:14 +0800

    first commit

---
+++ GestureLock/Bntview.h
@@ -0,0 +1,13 @@
+//
+//  Bntview.h
+//  GestureLock
+//
+//  Created by Superman on 2018/7/11.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Bntview : UIView
+
+@end
---
+++ GestureLock/Bntview.m
@@ -0,0 +1,21 @@
+//
+//  Bntview.m
+//  GestureLock
+//
+//  Created by Superman on 2018/7/11.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+#import "Bntview.h"
+
+@implementation Bntview
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
