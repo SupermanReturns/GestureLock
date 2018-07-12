7714c8969778f8f40ae6df5029f5f431e1c82cfe
Superman <703353105@qq.com>
2018-07-12 09:27:14 +0800

    修改代码

--- GestureLock/Bntview.h
+++ GestureLock/Bntview.h
@@ -7,7 +7,15 @@
 //
 
 #import <UIKit/UIKit.h>
+@class Bntview;
 
+@protocol BntviewDelegate <NSObject>
+-(void)bntview:(Bntview *)bntview :(NSString *)strM;
+
+@end
 @interface Bntview : UIView
+
+@property(nonatomic)id<BntviewDelegate> delegate;
+- (instancetype)initWithFrame:(CGRect)frame ;
 
 @end
--- GestureLock/ViewController.m
+++ GestureLock/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "Bntview.h"
 
-@interface ViewController ()
+
+@interface ViewController ()<BntviewDelegate>
+@property(strong,nonatomic)UIImageView *imageview;
+@property(strong,nonatomic)Bntview *bntview;
 
 @end
 
@@ -17,6 +21,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"Home_refresh_bg"]];
+    UIImageView *imageview=[[UIImageView alloc]initWithFrame:CGRectMake(([UIScreen mainScreen].bounds.size.width-100)*0.5, 100, 100, 100)];
+    [self.view addSubview:imageview];
+    _imageview=imageview;
+    
+    Bntview *bntview=[[Bntview alloc]initWithFrame:CGRectMake(([UIScreen mainScreen].bounds.size.width-300)*0.5, CGRectGetMaxY(imageview.frame)+50, 300, 300)];
+    bntview.delegate = self;
+    [self.view addSubview:bntview];
+    _bntview=bntview;
+    
+}
+//实现代理方法
+-(void)bntview:(Bntview *)bntview :(NSString *)strM{
+    //开启一个图形上下文
+    UIGraphicsBeginImageContextWithOptions(bntview.frame.size, NO, 0.0);
+    //获取图形上下文
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    //截图
+    [self.bntview.layer renderInContext:ctx];
+    //获取图片
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    //关闭图形上下文
+    UIGraphicsEndImageContext();
+    //把获取的图片保存到 imageview 中
+    self.imageview.image = image;
 }
 
 
@@ -27,3 +56,21 @@
 
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
